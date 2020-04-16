ec0ccef56659fa792bb65b1abe8824ac7fe22213
nathan-saygers <nathan.saygers@gmail.com>
2020-04-16 13:04:36 -0700

    stock prices and eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,19 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n >= 3:
+    return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+  if n == 2:
+    return eating_cookies(n - 2) + eating_cookies(n - 1)
+  if n < 2 and n > -1:
+    return 1
+
+# 1 => 1 (1)
+# 2 => 2 (1 * 2, 2 * 1)
+# 3 => 4 (1 * 3, 2 * 1 + 1 * 1,  1 * 1 +2 * 1,  3 * 1)
+# 4 => 5 (4 * 1, 1 * 4, 2 * 2, 3 * 1 + 1, 1 * 3 + 1, 
+#
+#
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
